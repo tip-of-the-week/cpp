bd56a3f67be13c259646714951cb84035cf5f96a
Alexandre Plateau <alexandre.plateau@nickel.eu>
2023-07-13 13:58:00 +0200

    Fixes order of the tips, their date, links formatting, removes empty p blocks

--- rss_generator.py
+++ rss_generator.py
@@ -5,24 +5,47 @@ import re
 import sys
 import glob
 import pathlib
-import datetime
+from datetime import datetime
 import markdown
 
 
+def sanitize(text: str) -> str:
+    return text.replace("<", "&lt;").replace(">", "&gt;")
+
+
 def gen_feed(tips, url):
-    date = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT")
     title_re = r"\* +(\*\*)?(?P<title>(?(1).+(?<!\*\*)|.+))(?(1)\*\*$|$)"
+    link_re = r"(?P<link>(?<!\[|\()https?://[\w\./\?&=\-\+#:,;%]+)"
 
     xml = ""
     for tip in tips:
-        title = re.search(title_re, tip["content"], re.RegexFlag.MULTILINE).group("title").replace("<", "&lt;").replace(">", "&gt;")
-
         link = tip["short_path"]
-        md = markdown.markdown(f"<div markdown=1>{tip['content']}</div>", extensions=["extra", "codehilite"], output_format="xhtml")
-        md = md.replace("<details open>", "<details open=true>").replace("<", "&lt;").replace(">", "&gt;")
+        tip_num = link.replace(".md", "")
+
+        date = datetime.fromtimestamp(tip["pub_date"]).strftime("%a, %d %b %Y %H:%M:%S GMT")
+
+        title = re.search(title_re, tip["content"], re.RegexFlag.MULTILINE).group("title")
+        title = sanitize(title)
+
+        content = tip["content"]
+        updated = content
+        for match in re.finditer(link_re, content):
+            naked_link = match.group("link")
+            updated = updated.replace(naked_link, f"[{naked_link}]({naked_link})")
+
+        # TODO: find a way to colorize the code, the documentation advise to have custom css
+        # The classes should already have been applied to the code thanks to extension 'codehilite'
+        md = markdown.markdown(
+                f"<div markdown=1>{updated}</div>",
+                extensions=["extra", "codehilite", "nl2br"],
+                output_format="xhtml"
+        )
+        md = md.replace("<p></p>", "")
+        md = md.replace("<details open>", "<details open=true>")
+        md = "<![CDATA[ %s]]>" % md
 
         xml += f"""<item>
-            <title>{title}</title>
+            <title>{tip_num} - {title}</title>
             <link>{url}{link}</link>
             <guid>{url}{link}</guid>
             <pubDate>{date}</pubDate>
@@ -33,12 +56,14 @@ def gen_feed(tips, url):
 
 def main():
     tips = []
+    files = sorted(glob.glob("tips/*.md"), reverse=True)
 
-    for file in glob.glob("tips/*.md"):
+    for file in files:
         path = pathlib.Path(file)
         tips.append({
             "content": path.read_text(encoding="utf-8"),
             "short_path": path.name,
+            "pub_date": path.lstat().st_ctime,
         })
 
     xml = gen_feed(tips, "https://github.com/tip-of-the-week/cpp/blob/master/tips/")
